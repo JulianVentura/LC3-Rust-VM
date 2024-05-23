5c74350da87a89d46f7015e16bbb416a475a45e2
Julian Ventura <julian.ventura@lambdaclass.com>
2024-05-23 15:55:03 -0300

    Add LC3VM skeleton

---
+++ src/lc3_vm.rs
@@ -0,0 +1,82 @@
+const MEMORY_MAX: usize = 1 << 16;
+const PC_START: u16 = 0x3000;
+//Warning, potential bug.
+//It would be nicer to have this directly linked to the REG enum
+const REG_COUNT: usize = 11;
+enum REG {
+    R0 = 0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    PC,
+    COND,
+    COUNT,
+}
+enum OP {
+    BR = 0,
+    ADD,
+    LD,
+    ST,
+    JSR,
+    AND,
+    LDR,
+    STR,
+    RTI,
+    NOT,
+    LDI,
+    STI,
+    JMP,
+    RES,
+    LEA,
+    TRAP,
+}
+
+enum FLAGS {
+    POS = 1 << 0,
+    ZRO = 1 << 1,
+    NEG = 1 << 2,
+}
+pub struct LC3VM {
+    memory: [u16; MEMORY_MAX],
+    reg: [u16; REG_COUNT],
+}
+
+impl LC3VM {
+    pub fn new() -> Self {
+        let mut vm = LC3VM {
+            memory: [0; MEMORY_MAX],
+            reg: [0; REG_COUNT],
+        };
+
+        vm.reg[REG::COND as usize] = FLAGS::ZRO as u16;
+        vm.memory[REG::PC as usize] = PC_START;
+        vm
+    }
+
+    pub fn run(&mut self, program: Vec<u16>) {
+        loop {
+            let instruction = self.read_instruction();
+            let opcode = self.parse_opcode(instruction);
+            match opcode {
+                op if op == OP::ADD as u16 => panic!("Add not implemented"),
+                _ => panic!("Not implemented"),
+            };
+        }
+    }
+
+    fn read_instruction(&mut self) -> u16 {
+        let pc = &mut self.reg[REG::COUNT as usize];
+        let instruction = self.memory[*pc as usize];
+        *pc += 1;
+
+        instruction
+    }
+
+    fn parse_opcode(&self, instruction: u16) -> u16 {
+        (instruction >> 12) as u16
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use lc3_rust_vm::lc3_vm;
+
 fn main() {
-    println!("Hello world!");
+    let _vm = lc3_vm::LC3VM::new();
 }
